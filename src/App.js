18c410b1d30cdfb9efe528c8f96fc0f1fa4b9ac8
Zuleyka Kohkemper <zulecr23@hotmail.com>
2021-07-29 19:38:04 -0600

    react components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
-import {Navbar, NavbarBrand} from 'reactstrap';
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from "./components/menuComponent";
+import { DISHES } from "./shared/dishes"
 
-function App() {
-  return (
-    <div>
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Ristorante con Fusion</NavbarBrand>
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+    render() {
+      return (
+        <div>
+          <Navbar dark color="primary">
+            <div className="container">
+              <NavbarBrand href="/">Ristorante con Fusion</NavbarBrand>
+            </div>
+          </Navbar>
+          <Menu dishes={this.state.dishes} />
         </div>
-      </Navbar>
-      <Menu/>
-    </div>
-  );
+      );
+    }
 }
 
 export default App;
